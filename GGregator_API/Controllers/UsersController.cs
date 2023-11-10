f843cf97c667906aa9945c476b668e7f0285c031
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-10 21:21:15 +0300

    Add UsersController class

---
+++ GGregator_API/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GGregator_API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
