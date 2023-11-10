f0fb7817193cf561e290b9502b5ec438bafb29f8
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-10 22:56:03 +0300

    Remove unnedeed (for now) Role field from User model

--- GGregator_Domain/Models/User.cs
+++ GGregator_Domain/Models/User.cs
@@ -2,15 +2,9 @@
 {
     public partial class User
     {
-        public enum UserRole
-        {
-            Standard,
-            Admin,
-        }
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public UserRole Role { get; set; }
 
     }
 }
