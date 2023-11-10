6d5ef7a369b18b29c1e66d82423af1fd246d2993
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-10 20:40:51 +0300

    Create User.cs

---
+++ GGregator_Domain/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGregator_Domain.Models
+{
+    public partial class User
+    {
+        public enum UserRole
+        {
+            Standard,
+            Admin,
+        }
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public UserRole Role { get; set; }
+
+    }
+}
