656a2acda23c16ba0d19a832a90d8c02148bdaab
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-10 21:33:08 +0300

    Add login and register DTOs
    they're the same lol

---
+++ GGregator_Domain/DTOs/RegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGregator_Domain.DTOs
+{
+    public class RegisterDTO
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
