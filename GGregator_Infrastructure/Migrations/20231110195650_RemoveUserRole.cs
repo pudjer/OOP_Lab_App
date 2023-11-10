da6b5f440fbfba88bede5ce54769069221e7a2c5
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-10 22:57:31 +0300

    Create migrations for removing Role field

---
+++ GGregator_Infrastructure/Migrations/20231110195650_RemoveUserRole.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GGregator_Infrastructure.Migrations
+{
+    public partial class RemoveUserRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
