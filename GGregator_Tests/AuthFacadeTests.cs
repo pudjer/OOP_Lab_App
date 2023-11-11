8bda4486202ed531ccbca821c9fb6e7b58ec152e
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-11 15:46:04 +0300

    Add first tests

---
+++ GGregator_Tests/AuthFacadeTests.cs
@@ -0,0 +1,30 @@
+using GGregator_Infrastructure.Facades;
+using Xunit;
+
+namespace GGregator_Tests
+{
+    public class AuthFacadeTests
+    {
+        [Fact]
+        public void HelperCheck()
+        {
+            var testHelper = new TestHelper();
+            IAuthenticationFacade authFacade = new AppAuthFacade();
+            // verify that the database and facade are created successfully
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async void Authenticate_ReturnsString_OnSuccessfulLogin()
+        {
+            var testHelper = new TestHelper();
+            IAuthenticationFacade authFacade = new AppAuthFacade();
+            var username = "test_user";
+            var password = "password";
+
+            var token = await authFacade.Authenticate(username, password);
+
+            Assert.IsType<string>(token);
+        }
+    }
+}
---
+++ GGregator_Tests/TestHelper.cs
@@ -0,0 +1,32 @@
+﻿using BCrypt.Net;
+using GGregator_Domain.Models;
+using GGregator_Infrastructure.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGregator_Tests
+{
+    internal class TestHelper
+    {
+        private readonly SQLiteContext _context;
+        public TestHelper()
+        {
+            var contextOptions = new DbContextOptionsBuilder<SQLiteContext>()
+                .UseSqlite("Data Source=Local.db").Options;
+            _context = new SQLiteContext(contextOptions);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var testUser = new User
+            {
+                Username = "test_user",
+                Password = BCrypt.Net.BCrypt.HashPassword("password")
+            };
+
+        }
+    }
+
+}
