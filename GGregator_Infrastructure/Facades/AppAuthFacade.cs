a849be6172e50bd71a624a6900544f123dbdf0c6
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-11 15:45:41 +0300

    Add IAuthenticationFacade implementation

---
+++ GGregator_Infrastructure/Facades/AppAuthFacade.cs
@@ -0,0 +1,22 @@
+﻿using GGregator_Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GGregator_Infrastructure.Facades
+{
+    public class AppAuthFacade : IAuthenticationFacade
+    {
+        public async Task<string?> Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserDTO?> Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
