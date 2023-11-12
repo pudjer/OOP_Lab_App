217be62b820d5a32703b93196aa7db8c8908fe7d
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-12 22:14:05 +0300

    Swap methods in the auth facade interface as well

--- GGregator_Infrastructure/Facades/IAuthenticationFacade.cs
+++ GGregator_Infrastructure/Facades/IAuthenticationFacade.cs
@@ -4,8 +4,8 @@ namespace GGregator_Infrastructure.Facades
 {
     public interface IAuthenticationFacade
     {
-        public Task<string?> Authenticate(string username, string password);
-
         public Task<UserDTO?> Register(string username, string password);
+
+        public Task<string?> Authenticate(string username, string password);
     }
 }
