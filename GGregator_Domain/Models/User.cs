177b3e3ec0cb5adfce32944b7c273438e99de6cd
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-11-17 12:08:23 +0300

    Remove the Subscription model

--- GGregator_Domain/Models/User.cs
+++ GGregator_Domain/Models/User.cs
@@ -5,7 +5,5 @@
         public Guid Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-
-        public List<Subscription> Subscriptions { get; set; } = new();
     }
 }
--- GGregator_Infrastructure/DbContexts/AppDbContext.cs
+++ GGregator_Infrastructure/DbContexts/AppDbContext.cs
@@ -23,6 +23,5 @@ namespace GGregator_Infrastructure.DbContexts
         }
 
         public virtual DbSet<User> Users { get; set; }
-        public virtual DbSet<Subscription> Subscriptions { get; set; }
     }
 }
