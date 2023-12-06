cda9721d9bcd01e19405689a406b93eeee23a67b
Yarosl-M <93162306+Yarosl-M@users.noreply.github.com>
2023-12-06 13:58:49 +0300

    Create UserDTO.cs

---
+++ AT_Domain/DTOs/OutDTOs/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT_Domain.DTOs.OutDTOs
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string IsAdmin { get; set; }
+        public string IsSubscribed { get; set; }
+    }
+}
